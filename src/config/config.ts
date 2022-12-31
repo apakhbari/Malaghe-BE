e71b5700d416c27d57567d28458528300e1e82a0
Rhodium.5135 <apakhbari@gmail.com>
2022-12-31 18:01:13 +0330

    minor deployment bug fixes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,17 @@
 module.exports = {
-  MONGO_IP: process.env.MONGO_IP || 'mongo',
-  MONGO_PORT: process.env.MONGO_PORT || 27017,
+  MONGO_URI: 'mongodb://mongo:27017/mongo' || process.env.MONGO_URI,
+
+  MONGO_IP: 'mongo' || process.env.MONGO_IP,
+  MONGO_PORT: 27017 || process.env.MONGO_PORT,
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
 
-  REDIS_URL: process.env.REDIS_URL || 'redis',
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
+  JWT_KEY: 12345678 || process.env.JWT_KEY,
 
-  SESSION_SECRET: process.env.SESSION_SECRET,
+  REDIS_URL: 'redis' || process.env.REDIS_URL,
+  REDIS_PORT: 6379 || process.env.REDIS_PORT,
+
+  SESSION_SECRET: 'secret' || process.env.SESSION_SECRET,
+
+  NODE_ENV: 'development' || process.env.NODE_ENV,
 }
