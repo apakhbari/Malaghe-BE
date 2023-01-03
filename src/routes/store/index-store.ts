91ccceec893e470afc85646acc884984eb82b7c6
Rhodium.5135 <apakhbari@gmail.com>
2023-01-03 15:52:53 +0330

    store modified

--- src/routes/store/index-store.ts
+++ src/routes/store/index-store.ts
@@ -1,17 +1,22 @@
 import express, { Request, Response } from 'express'
 import { Store } from '../../models/store'
+import { currentUser } from '../../middleware/current-user'
 
 const router = express.Router()
 
-router.get('/api/v1/store', async (req: Request, res: Response) => {
-  const stores = await Store.find(
-    {},
-    'title id summary createdAt price hasDiscount discountKind discountedPrice madeIn goodKind hasMag'
-  )
-    .sort('-createdAt')
-    .limit(10)
+router.get(
+  '/api/v1/store',
+  currentUser,
+  async (req: Request, res: Response) => {
+    const stores = await Store.find(
+      {},
+      'title id summary createdAt price hasDiscount discountKind discountedPrice madeIn goodKind hasMag'
+    )
+      .sort('-createdAt')
+      .limit(10)
 
-  res.send(stores)
-})
+    res.send(stores)
+  }
+)
 
 export { router as indexStoreRouter }
